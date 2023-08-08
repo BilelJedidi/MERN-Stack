8c5ac02c681007105bf13601f62e6384c7836597
Mehdi Jedidi <bileljedidi98@gmail.com>
2023-08-08 18:06:36 +0100

    FirstReact

--- Algo/indexOf.js
+++ Algo/indexOf.js
@@ -24,7 +24,6 @@ const expected4 = -1;
  */
 function indexOf(items, searchItem) {
     // code here
-    var expected = 0
     for (var i=0; i<items.length; i++){
         if (items[i] === searchItem){
             return i
