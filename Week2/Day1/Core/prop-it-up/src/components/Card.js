439de3869ab334ddb8f280e08e49f203833198ba
Mehdi Jedidi <bileljedidi98@gmail.com>
2023-08-09 11:16:41 +0100

    edit

--- Week2/Day1/Core/prop-it-up/src/components/Card.js
+++ Week2/Day1/Core/prop-it-up/src/components/Card.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const PersonCard = (props) => {
-  // destructuring the props makes it easier to use in the component
-  //    but is NOT required
-  // We will not destructure age from props and will use it directly
-  //    in the JSX that is returned to demonstrate a different way to
-  //    use props
+
   const { firstName, lastName, hair } = props;
 
   return (
